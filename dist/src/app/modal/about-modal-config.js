e0a5c7788a1132c35f9e80a8d3b650c696fdc888
sahil143 <sahil143@redhat.com>
2017-11-13 06:53:20 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/modal/about-modal-config.js
@@ -0,0 +1,10 @@
+/**
+ * A config containing properties for About Modal
+ */
+var AboutModalConfig = (function () {
+    function AboutModalConfig() {
+    }
+    return AboutModalConfig;
+}());
+export { AboutModalConfig };
+//# sourceMappingURL=about-modal-config.js.map
